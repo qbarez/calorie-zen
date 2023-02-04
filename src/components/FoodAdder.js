708e76f112547c44f99ef440978c5fb64a7b3f0d
Valeriy Gogin <qbarez@yandex.ru>
2023-02-04 22:14:58 +0300

    feat form

--- src/components/FoodAdder.js
+++ src/components/FoodAdder.js
@@ -5,18 +5,31 @@ class FoodAdder extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      foodList: []
+      food: '',
+      calories: ''
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit = () => {
+  handleChange = (e) => {
+   this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleSubmit(this.state.food, this.state.calories);
+    this.setState();
   }
 
   render() {
+    // добавьте элементами обработчики handleSubmit и handleChange
     return (
-      <form className="food-adder">
-        <input type="text" placeholder="Добавьте продукты" />
+      <form className="food-adder" onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} name="food" value={this.state.food} type="text" required placeholder="Добавьте продукты" />
+        <input onChange={this.handleChange} name="calories" value={this.state.calories} type="number" required placeholder="Калории" />
         <button className="food-adder__submit"></button>
       </form>
     );
--- src/components/Tips.js
+++ src/components/Tips.js
@@ -6,16 +6,17 @@ function Tips() {
     fetch('https://api.nomoreparties.co/todays-tips-rus').then((res) => {
       return res.json();
     }).then((res) => {
-      setList(Object.values(res));  
+      setList(Object.values(res));
     })
   }, []);
   const [list, setList] = useState();
   return (
     <div className="tips">
       <ul className="tips__list">
-      {list && list.map((item, i) => {
-       return (<li key={item.id} className="tips__item">{item.tip}</li>)
-      })
+      {list &&
+        list.map((item, i) => {
+          return (<li key={i} className="tips__item">{item.tip}</li>)
+        })
       }
       </ul>
     </div>
